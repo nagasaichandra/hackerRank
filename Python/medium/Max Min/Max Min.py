15577feb036fcdd08eebc5cc31095ce3cdc53182
nagasaichandra <nagasaichandra@gmail.com>
2021-04-23 14:54:38 -0700

    max min

---
+++ Python/medium/Max Min/Max Min.py
@@ -0,0 +1,47 @@
+# https://www.hackerrank.com/challenges/angry-children/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    # Write your code here
+    arr = sorted(arr)
+    d = math.inf
+    for i in range(0, len(arr)-k+1):
+        diff = arr[i+k-1] - arr[i]
+        if diff < d:
+            d = diff
+    return d
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
